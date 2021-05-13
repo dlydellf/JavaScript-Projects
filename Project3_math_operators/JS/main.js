7100dd747eed7c6ff1e9fbac163e93100e68e3e5
DForeman <dlydellf@hotmail.com>
2021-05-12 21:33:04 -0500

    Added a subtraction function to .js, with result going to the .html's DOM

--- Project3_math_operators/JS/main.js
+++ Project3_math_operators/JS/main.js
@@ -4,3 +4,10 @@ function randomNumb() {
   var result = ((num1 + num2) * num1) / num2;
   document.getElementById("Math").innerHTML = result;
 }
+
+function subtraction() {
+  var numA = Math.floor(Math.random() * 11) + 1; // Random integer between 1 & 10
+  var numB = Math.floor(Math.random() * 3) + 1; // Random integer between 1 & 2
+  var difference = numA - numB; // subtracts 1st numb from 2nd num
+  document.getElementById("Subtraction").innerHTML = difference; // Places the result in the DOM
+}
