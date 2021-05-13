1062df9319f8c3c0ecea828acbe5f864e0b58150
DForeman <dlydellf@hotmail.com>
2021-05-12 19:38:28 -0500

    Created main.js for Steps 37-63; updated .html file per Step 35

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,34 @@
+//Window Alert (Step 37):
+window.alert("Hello, World!");
+
+//Document Write (Step 39):
+document.write("Hello, World!");
+
+//String Assignment (Steps 43 & 46):
+var A = "Assigning this string value to the variable A.";
+window.alert(A);
+
+//Escape Character Assignment (Step 48):
+var B = "This string contains both apostrophes ('s) AND quotation marks (\"s).";
+window.alert(B);
+
+//Concatenation Assignment (Step 50):
+document.write("  I'm concatenating the strings: '" + A + "', with the string: '" + B + "'.");
+
+//Concatenated String Challenge (Step 51):
+var C = "This is two concatenated " + "strings. ";
+document.write(C);
+
+//Multiple Variables Assignment & Colors Assignment (Steps 53 & 55):
+var a = "First";
+var a = a.fontcolor("red");
+var b = "Second";
+var b = b.fontcolor("blue");
+var c = "Third";
+var c = c.fontcolor("brown");
+var d = "Fourth";
+var d = d.fontcolor("green");
+document.write(d);
+
+//Expression Assignment (Step 57):
+//1 + 4;
