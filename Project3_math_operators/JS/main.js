c0d792bc163a2551f189b0d16c440d45be0ba83c
DForeman <dlydellf@hotmail.com>
2021-05-13 01:58:07 -0500

    Added multi-operator function per Step 84 - NOT g2g

--- Project3_math_operators/JS/main.js
+++ Project3_math_operators/JS/main.js
@@ -10,7 +10,7 @@ function randomNum() {
 
 // Step 80
 function subtraction() {
-  var numB = Math.floor(Math.random() * 20) + 1; // Random integer between 1 & 20
+  var numB = Math.floor(Math.random() * 21) + 1; // Random integer between 1 & 20
   var numA = numB * 2 + 3; // Random integer
   var difference = numA - numB; // subtracts 1st num from 2nd num
   document.getElementById("Subtraction").innerHTML = `${numA} - ${numB} = <strong>${difference}<strong>`; // Places the result in the DOM
@@ -24,8 +24,25 @@ function multiplication() {
   document.getElementById("Multiplication").innerHTML = `${numA} x ${numB} = <strong>${product}<strong>`;
 }
 function division() {
-  var dividend = Math.floor(Math.random() * 100) + 1; // Random integer between 1 & 100
+  var dividend = Math.floor(Math.random() * 101) + 1; // Random integer between 1 & 100
   var divisor = Math.floor(Math.random() * 6) + 1; // Random integer between 1 & 5
   const quotient = dividend / divisor;
-  document.getElementById("Division").innerHTML = `${dividend} &#247 ${divisor} = ${quotient}`;
+  document.getElementById("Division").innerHTML = `${dividend} &#247 ${divisor} = <strong>${quotient}<strong>`;
+}
+
+// Step 84
+function allAtOnce() {
+  var randNumb = Math.floor(Math.random() * 4) + 1; // Random integer between 1 & 3
+  document.getElementById("RandNumb").innerHTML = `<strong>${randNumb}<strong>`;
+  var result = randNumb;
+  while (result != 1) {
+    if (result % 2 == 0) {
+      var result = randNumb / 2;
+      document.getElementById("Evens").innerHTML = `${result}`;
+    } else {
+      var result = randNumb * 3 + 1;
+      document.getElementById("Odds").innerHTML = `${result}`;
+    }
+    document.getElementById("Results").innerHTML = `<strong>${result}<strong>`;
+  }
 }
