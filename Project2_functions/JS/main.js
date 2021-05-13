e45451c6ce5344938b973bcbf9dc795818e657e0
DForeman <dlydellf@hotmail.com>
2021-05-12 20:16:14 -0500

    Added a 2nd event handler to the .html & a function within the .js per Step 67

--- Project2_functions/JS/main.js
+++ Project2_functions/JS/main.js
@@ -3,3 +3,9 @@ function notMyFirstFunction() {
   var var2 = var1.toUpperCase();
   document.getElementById("favDay").innerHTML = var2;
 }
+
+function step67() {
+  var result = "This string... ";
+  result += "was concatenated!";
+  document.getElementById("step67").innerHTML = result;
+}
