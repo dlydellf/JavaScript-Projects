e640ba34bdb9d769baa3e7b71c466a628f5225d2
DForeman <dlydellf@hotmail.com>
2021-05-13 22:26:08 -0500

    Overhauled allAtOnce()'s code block & comments; added 2 helper functions to better handle output mssgs

--- Project3_math_operators/JS/main.js
+++ Project3_math_operators/JS/main.js
@@ -31,24 +31,53 @@ function division() {
 }
 
 // Steps 84, 86 & 92
-function allAtOnce() {
-  var randNumb = Math.floor(Math.random() * 4) + 1; // Random integer between 1 & 3
-  document.getElementById("RandNumb").innerHTML = `<strong>${randNumb}<strong>`; // >>>>> DOM is updated with the chosen random number
-  var result = randNumb; // Declare & assign a 2nd variable equal to the random number
-  while (result != 1) {
-    // While the 2nd variable is NOT 1...
-    if (result % 2 == 0) {
-      // Check if the 2nd variable is even
-      var result = randNumb / 2; // Halve random number & reassign to the 2nd variable
-      document.getElementById("Evens").innerHTML = `${result}`; // >>>>> DOM is updated with result from Line 40, GOTO to Line 38
-    } else {
-      var result = randNumb * 3 + 1; // If 2nd variable is ODD, multiply by 3 and add 1
-      document.getElementById("Odds").innerHTML = `${result}`; // >>>>> DOM is updated with result from Line 43, GOTO Line 38
-    }
-    document.getElementById("Results").innerHTML = `<strong>${result}<strong>`; // >>>>> Once 2nd variable == 1, update DOM & END WHILE LOOP!!
-    result = 1;
+function firstHelper(result) {
+  if (result % 2 == 0) {
+    var result2 = result / 2;
+    return `(${result} is <em>EVEN</em>, so "${result} &#247 2" = <strong>${result2}</strong>, ${lastHelper(result2)})`;
+  } else {
+    var result2 = result * 3 + 1;
+    return `(${result} is <em>ODD</em>, so "${result} x 3" is ${
+      result * 3
+    }, and + 1 = <strong>${result2}</strong>, ${lastHelper(result2)})`;
   }
 }
+function lastHelper(result2) {
+  if (result2 % 2 == 0) {
+    return `...which is <em>EVEN</em>, so again, "${result2} &#247 2" = <strong>${result2 / 2}</strong>...`;
+  } else {
+    return `...which is <em>ODD</em>, so again we do "${result2} x 3", getting ${
+      result2 * 3
+    }, and adding 1 again = <strong>${result2 * 3 + 1}</strong>...`;
+  }
+}
+
+function allAtOnce() {
+  var randNumb = Math.floor(Math.random() * 51) + 2; // Random integer between 2 & 50
+  document.getElementById(
+    "RandNumb",
+  ).innerHTML = `<li>A number between 2 and 50 was randomly chosen: <br><strong>${randNumb}</strong></li><ul id="Evens"></ul><ul id="Odds"></ul><br><li id="Results"></li>`; // >>>>> DOM is updated with the chosen random number & prepped to display function's next output.
+  var result = randNumb; // Declare & assign a 2nd variable equal to the random number
+  while (result != 1) {
+    // As long as the 2nd variable is NOT 1...
+    if (result % 2 == 0) {
+      // ...check if the 2nd variable is even.
+      var result = randNumb / 2; // If the random number IS even, halve it & reassign the result to the 2nd variable.
+      document.getElementById("Evens").innerHTML = `<li>If even, &#247 2:<br>${result}</li>`; // >>>>> Update DOM with previous line's result.
+      document.getElementById("Odds").innerHTML = `<li><s>If odd, x3 and then +1:</s></li>`; // User needs to see this to follow function's purpose/logic!
+    } else {
+      var result = randNumb * 3 + 1; // If 2nd variable is ODD, multiply by 3 and add 1
+      document.getElementById("Odds").innerHTML = `<li>If odd, x 3 and then +1:<br>${result}</li>`; // >>>>> Update DOM with previous line's result.
+      document.getElementById("Evens").innerHTML = `<li><s>If even, &#247 2:</s></li>`; // User needs to see this to follow function's purpose/logic!
+    }
+    document.getElementById(
+      "Results",
+    ).innerHTML = `Keep repeating these steps with the results from above:<br>${firstHelper(
+      result,
+    )}<br><br>...and you'll eventually reach the number "1"!<br>(This math gem works regardless of <em>HOW LARGE</em> your starting number is!)`; // >>>>> GOTO while loop's entrance
+    result = 1; // Thanks to Ben, was added to END WHILE LOOP!
+  }
+} // Function doesn't continuously calculate down to 1, yet.
 
 // Steps 88 & 92:
 function negation() {
@@ -63,7 +92,7 @@ function increment() {
   newRandNum++;
   document.getElementById(
     "Increment",
-  ).innerHTML = `The value ${randNum} was <em>incremented</em> (by one) to ${newRandNum}.`;
+  ).innerHTML = `The value ${randNum} was <em>incremented</em> (by one) to <strong>${newRandNum}</strong>.`;
 }
 function decrement() {
   var randNumb = Math.floor(Math.random() * 101) + 1; // Random integer between 1 & 100
@@ -71,13 +100,17 @@ function decrement() {
   newRandNumb--;
   document.getElementById(
     "Decrement",
-  ).innerHTML = `The value ${randNumb} has been <em>decremented</em> (by one) to ${newRandNumb}.`;
+  ).innerHTML = `The value ${randNumb} has been <em>decremented</em> (by one) to <strong>${newRandNumb}</strong>.`;
 }
 
 // Step 93
 function mathObjectMethod() {
-  var randBase = Math.floor(Math.random() * 11) + 1; // Random integer (for the base) between 1 & 10
+  var randBase = Math.floor(Math.random() * 16) + 1; // Random integer (for the base) between 1 & 15
   var randExp = Math.floor(Math.random() * 4) + 1; // Random integer (for the exponent) between 1 & 3
-  document.getElementById("MathObjectMethod").innerHTML =
-    `A base of ${randBase}, raised to the power of ${randExp} = ` + Math.pow(randBase, randExp);
+  document.getElementById(
+    "MathObjectMethod",
+  ).innerHTML = `A base of <u>${randBase}</u>, raised to the power of <u>${randExp}</u>, equals <strong>${Math.pow(
+    randBase,
+    randExp,
+  )}</strong>.`;
 }
