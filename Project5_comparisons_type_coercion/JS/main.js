a90852a8dcf4f1260afa99441481eee4d514c262
DForeman <dlydellf@hotmail.com>
2021-05-14 18:45:46 -0500

    Updated the .html to show all steps of the NaNCallenge()

--- Project5_comparisons_type_coercion/JS/main.js
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -2,11 +2,13 @@
 document.write(typeof "Douglas"); // Practice using the "typeof" operator to display the data types of varilables of my choosing
 document.write(typeof 2021);
 document.write(typeof true);
-document.write(typeof false);
 document.write(typeof null);
+document.write(typeof NaN);
 
 //Step 105:
 document.write(1 + 1);
 document.write("1" + 1);
 document.write("one" + 1);
 document.write("one" + "one");
+
+//Step 108:
