bf6ed98e8ad30370f91b2a94612a2994e91e67a6
DForeman <dlydellf@hotmail.com>
2021-05-14 14:45:09 -0500

    "Typeof" operator used to display the data types of 5 variables

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,6 @@
+//Step 104:
+document.write(typeof "Douglas"); // Practice using the "typeof" operator to display the data types of varilables of my choosing
+document.write(typeof 2021);
+document.write(typeof true);
+document.write(typeof false);
+document.write(typeof null);
