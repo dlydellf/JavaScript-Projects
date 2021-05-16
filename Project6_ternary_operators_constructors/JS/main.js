5a04bd77e706996e467da09fdb8d563a360612c6
DForeman <dlydellf@hotmail.com>
2021-05-15 23:04:37 -0500

    Created the .html & .js files for the Ternary Project

---
+++ Project6_ternary_operators_constructors/JS/main.js
@@ -0,0 +1,6 @@
+function Ride_Function() {
+  var Height, Can_ride;
+  Height = document.getElementById("Height").value;
+  Can_ride = Height < 52 ? "You are too short" : "You are tall enough";
+  document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
+}
