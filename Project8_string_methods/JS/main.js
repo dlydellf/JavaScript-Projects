2b8874c03a709b13c1f969fa45c3d7dd8b32b7af
DForeman <dlydellf@hotmail.com>
2021-05-16 18:10:53 -0500

    Added slice method to .js - g2g

--- Project8_string_methods/JS/main.js
+++ Project8_string_methods/JS/main.js
@@ -1,7 +1,13 @@
+// Step 156:
 function concatenate() {
   var beginning = "This sentence ";
   var middle = "is actually 3 strings that were";
   var end = " concatenated together, onto one.";
-  var result = beginning.concat(middle, end);
+  var result = beginning.concat(middle, end); // The concate method, combining all three strings into one
   document.getElementById("Concatenate").innerHTML = result;
+}
+// Step 158:
+function slice() {
+  var string = document.getElementById("PreSlice").innerHTML;
+  document.getElementById("PostSlice").innerHTML = string.slice(12, 20);
 }
