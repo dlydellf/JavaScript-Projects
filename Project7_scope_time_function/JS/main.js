99b414fca229bcee6f2a36d18f7df543667cb4b5
DForeman <dlydellf@hotmail.com>
2021-05-16 16:02:40 -0500

    Added Time_function() - g2g

--- Project7_scope_time_function/JS/main.js
+++ Project7_scope_time_function/JS/main.js
@@ -42,3 +42,17 @@ function my2ndIf() {
   document.getElementById("TheOutput").innerHTML = `You chose <strong>${number}</strong>, an ${reply} number.`;
   // Return output
 }
+
+// Steps 149 & 150:
+function Time_function() {
+  var Time = new Date().getHours();
+  var Reply;
+  if (Time < 12 == Time > 0) {
+    Reply = "It is morning time!";
+  } else if (Time >= 12 == Time < 18) {
+    Reply = "It is afternoon.";
+  } else {
+    Reply = "It is evening time.";
+  }
+  document.getElementById("Time_of_day").innerHTML = Reply;
+}
