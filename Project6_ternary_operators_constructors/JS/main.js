cb014fd288106371db0e618b0868645dd9290074
DForeman <dlydellf@hotmail.com>
2021-05-16 00:10:01 -0500

    Added Step 131's code, per instructions

--- Project6_ternary_operators_constructors/JS/main.js
+++ Project6_ternary_operators_constructors/JS/main.js
@@ -13,3 +13,18 @@ function oldEnough() {
   var Can_vote = Age >= 18 ? "Congratulations!  You are old enough" : "Sorry, but you are too young";
   document.getElementById("Vote").innerHTML = `${Can_vote} to vote!`;
 }
+
+// Step 132:
+function Vehicle(Make, Model, Year, Color) {
+  this.Vehicle_Make = Make;
+  this.Vehicle_Model = Model;
+  this.Vehicle_Year = Year;
+  this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myFunction() {
+  document.getElementById("Keywords_and_Constructors").innerHTML =
+    "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;
+}
