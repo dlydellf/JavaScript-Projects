114b69745ff05a91e14f41a5ef42008511ed5828
DForeman <dlydellf@hotmail.com>
2021-05-15 23:29:32 -0500

    Updated .js with the Voter Program - g2g

--- Project6_ternary_operators_constructors/JS/main.js
+++ Project6_ternary_operators_constructors/JS/main.js
@@ -1,6 +1,15 @@
+// Step 127:
 function Ride_Function() {
+  // this function was provided in the instructions
   var Height, Can_ride;
   Height = document.getElementById("Height").value;
   Can_ride = Height < 52 ? "You are too short" : "You are tall enough";
   document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
+}
+
+// Step 128:
+function oldEnough() {
+  var Age = document.getElementById("Age").value;
+  var Can_vote = Age >= 18 ? "Congratulations!  You are old enough" : "Sorry, but you are too young";
+  document.getElementById("Vote").innerHTML = `${Can_vote} to vote!`;
 }
