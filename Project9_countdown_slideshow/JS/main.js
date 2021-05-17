9c4095a32ceb1bf8322d5b3916f50ce46062c17d
DForeman <dlydellf@hotmail.com>
2021-05-17 16:43:48 -0500

    Updated countdown() so countdown doesn't continue (and isn't off by a second)

--- Project9_countdown_slideshow/JS/main.js
+++ Project9_countdown_slideshow/JS/main.js
@@ -1,12 +1,13 @@
 function countdown() {
-  var seconds = document.getElementById("sceonds").value;
+  var seconds = document.getElementById("seconds").value;
 
   function tick() {
-      seconds = seconds -1;
+    if (seconds == 0) {
+      alert("Time's up!");
+    } else {
+      seconds = seconds - 1;
       timer.innerHTML = seconds;
       setTimeout(tick, 1000);
-    if(seconds -- =1) {
-      alert("Time's up!")
     }
   }
   tick();
