0138c9f6fa0b161d3d9837a6e59cb06f8c52c124
DForeman <dlydellf@hotmail.com>
2021-05-17 21:51:16 -0500

    Added Call_Loop() - g2g

---
+++ Project10_loops_arrays/JS/main.js
@@ -0,0 +1,9 @@
+function Call_Loop() {
+  var digit = "";
+  var timer = 10;
+  while (timer > 0) {
+    digit += `<br> ${timer}...`;
+    timer--;
+  }
+  document.getElementById("Loop").innerHTML = `T-minus ${digit} until launch`;
+}
