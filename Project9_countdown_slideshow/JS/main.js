c2fd3789bbb7c712c4821b3089532f62a1be5c16
DForeman <dlydellf@hotmail.com>
2021-05-17 20:08:38 -0500

    Updated .js with code automating slideshow - works, but not 100% g2g

--- Project9_countdown_slideshow/JS/main.js
+++ Project9_countdown_slideshow/JS/main.js
@@ -15,8 +15,8 @@ function countdown() {
 }*/
 // Code above is for the COUNTDOWN; code below is for the SLIDESHOW
 
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = 0;
+showSlides(); // console TypeError re: this line (and line 65)
 
 // Next/previous controls
 function plusSlides(n) {
@@ -46,4 +46,23 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
+}
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block"; // console TypeError re: this line (and line 19)
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
