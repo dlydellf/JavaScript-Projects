dd313a2500799b7cabf6dafccbb5e993f899ef1d
DForeman <dlydellf@hotmail.com>
2021-05-17 22:00:55 -0500

    Added String_Length() to .js - g2g

--- Project10_loops_arrays/JS/main.js
+++ Project10_loops_arrays/JS/main.js
@@ -1,3 +1,4 @@
+// Step 199:
 function Call_Loop() {
   var digit = "";
   var timer = 10;
@@ -6,4 +7,12 @@ function Call_Loop() {
     timer--;
   }
   document.getElementById("Loop").innerHTML = `T-minus ${digit} until launch`;
+}
+
+// Step 200:
+function String_Length() {
+  var string = "I bet you're reading this sentence";
+  document.getElementById(
+    "Length",
+  ).innerHTML = `The length of the string "${string}" is:<br><u><strong>${string.length}</strong></u> characters.`;
 }
