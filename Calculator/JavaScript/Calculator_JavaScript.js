293bf012fb1c3d3fffc4b2fe03b418e7e89a8f0e
DForeman <dlydellf@hotmail.com>
2021-05-22 17:32:16 -0500

    Added Input_Decimal() - defines operation of "." button

--- Calculator/JavaScript/Calculator_JavaScript.js
+++ Calculator/JavaScript/Calculator_JavaScript.js
@@ -12,17 +12,30 @@ const Calculator = {
 
 // Modifies values each time a button is pressed:
 function Input_Digit(digit) {
+  // Destructures two properties from the "Calculator" object:
   const {
     Display_Value, Wait_Second_Operand
   } = Calculator
-  /*  Checks if "Wait_Second_Operand" == TRUE, and sets "Display_Value"
+  /* If Wait_Second_Operand === TRUE, Display_Value is set
   to the key that was clicked */
   if (Wait_Second_Operand === true) {
     Calculator.Display_Value = digit
     Calculator,.Wait_Second_Operand = false
   } else {
-    /* Overwrites Display_Value if the current value is "0":
+    /* Overwrites Display_Value if its current value is "0":
     otherwise, it adds onto it: */
     Calculator.Display_Value = Display_Value === "0" ? digit : Display_Value + digit
+  }
+}
+
+//This section handles decimal points:
+function Input_Decimal(dot) {
+  /* Ensures that accidentially clicking the decimal point
+  doesn't cause bugs in your operation: */
+  if (Calculator.Wait_Second_Operand === true) 
+    return;
+  if (!Calculator.Display_Value.includes(dot)) {
+    // if Display_Value doesn't already contain a decimal point, add one
+    Calculator.Display_Value += dot
   }
 }
