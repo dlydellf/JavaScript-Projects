8fadbd0518bc3a7f02b1ec9661a8a60c320eea78
DForeman <dlydellf@hotmail.com>
2021-05-22 22:10:35 -0500

    Updated .js w/formatting to correct object syntax - 100% g2g

--- Calculator/JavaScript/Calculator_JavaScript.js
+++ Calculator/JavaScript/Calculator_JavaScript.js
@@ -1,11 +1,11 @@
 // Creates an object that keeps track of values:
 const Calculator = {
   // Displays "0" on the screen:
-  Display_Value: "0"
+  Display_Value: "0",
   // Holds the 1st operand for any expressions; set to "NULL" for now:
-  First_Operand: null
+  First_Operand: null,
   // Checks whether or not the 2nd operand has been input:
-  Second_Operand: false
+  Second_Operand: false,
   // Holds the operator; set to "NULL" for now:
   operator: null
 }
