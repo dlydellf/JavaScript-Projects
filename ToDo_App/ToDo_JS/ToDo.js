fc49e19655ca04a6c1ed00dfd4942093b4a108f5
DForeman <dlydellf@hotmail.com>
2021-05-31 13:18:39 -0500

    Per Step 421, added feature to remove taks from ToDo list - 100% g2g

--- ToDo_App/ToDo_JS/ToDo.js
+++ ToDo_App/ToDo_JS/ToDo.js
@@ -4,7 +4,7 @@ function get_todos() {
   var todos = new Array();
   // This pulls the task that was saved in the web browser's memory:
   var todos_str = localStorage.getItem("todo");
-  /* If !input == null, then "JSON.parse()" will communicate with the web browser to make the task a JS object: */
+  /* If input !== null, then "JSON.parse()" will communicate with the web browser to make the task a JS object: */
   if (todos_str !== null) {
     todos = JSON.parse(todos_str);
   }
@@ -26,20 +26,39 @@ function add() {
 
 // This function keeps the tasks permanetly displayed on the screen:
 function show() {
-  // This sets the task that was retrieved as a vaariable:
+  // This sets the task that was retrieved as a variable:
   var todos = get_todos();
   // This sets up each task as an unordered list:
   var html = "<ui>";
   // This displays a task to the list in the order that it was inputed:
   for (var i = 0; i < todos.length; i++) {
     /* This also displays the task as a list, and creates the button with the "x": */
-    html += `<li> ${todos[i]} <button class="remove" id= ${[i]} >x</button></li>`;
+    html += `<li> ${todos[i]}. <button class="remove" id=${[i]}>x</button></li>`;
   }
   html += "</ui>";
   // This displays the task as a list:
   document.getElementById("todos").innerHTML = html;
+  /* THis tells the browser how to display the todo array after an item has been removed: */
+  var buttons = document.getElementsByClassName("remove");
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", remove);
+  }
 }
 // This displays the inputed task when the "Add Item" button is clicked:
 document.getElementById("add").addEventListener("click", add);
 // This will keep the inputs displayed permanently on the screen:
 show();
+
+/* ==============================================
+    Step 421 ToDo Assignment
+  ============================================ */
+function remove() {
+  var id = this.getAttribute("id");
+  var todos = get_todos();
+  todos.splice(id, 1);
+  localStorage.setItem("todo", JSON.stringify(todos));
+  /* This looks in the show() how to display a removed item on the screen: */
+  show();
+
+  return false;
+}
